146b92b10a2c4aa3523d80e37b31f38d6a99fb6e
gouri <gourishankar13@gmail.com>
2014-08-13 21:09:14 +0600

    Adding unit testing and modify the script

--- presentationScheduler.py
+++ presentationScheduler.py
@@ -8,10 +8,11 @@
 #-------------------------------------------------------------------------------
 
 import csv, os
+import itertools
 
-def getRow():
-    if os.path.exists(CSV_DATA_FILE):
-        data = csv.reader(open(CSV_DATA_FILE))
+def getRow(csvFile):
+    if os.path.exists(csvFile):
+        data = csv.reader(open(csvFile))
         fields = data.next()
         for echRow in data:
             try:
@@ -22,172 +23,70 @@ def getRow():
             yield echRow
 
 
-def maxPresenter(data):
-    '''
-     Maximize the number of presenters -
-     Select the case that fits in maximum number of presenters in the given time
-     schedule. If multiple cases satisfy this scenario,
-     select the ones with minimum cost.
+def findCombination(mxPresenterNum, data, totDur):
+    return filter(lambda x: sum([ei[1] for ei in x]) == totDur, list(itertools.combinations(data, mxPresenterNum)))
 
-    data : a list of [<presenterName>, <presentationHours><int>, <cost><int>]
-    '''
-    gotSlotList = []
+def sortData(data):
+    return sorted(data, key=lambda x: sum([int(i[2]) for i in x]))
 
+def getMaxPresenterCombination(totSlot, confDuration, csvFile):
+    data = list(getRow(csvFile))
+    result = []
+    for mxPresenterNum in range(totSlot, 0, -1):
+        result = findCombination(mxPresenterNum, data, confDuration)
+        if result:
+            result = sortData(result)
+            # As got max presenter
+            break
+    lstCost = None
+    res = []
+    for echSet in result:
+        tmp = [0]
+        for echPresenter in echSet:
+            tmp.insert(-1, echPresenter[0])
+            tmp[-1] += echPresenter[2]
+        if lstCost == None:
+            lstCost = tmp[-1]
+        elif lstCost < tmp[-1]:
+            break
+        res.append(tmp)
 
-    def __iscontinue(totCst, curLn, totDr, slotLst):
-        if totDr > CONFERENCE_DURATION:
-            return True
-        elif totDr < CONFERENCE_DURATION:
-            return False
-        slotLst.append(totCst)
-        # There is any data in gotSlotList only then:
-        # check for low cost and remove if the previous costs are higher
-        if gotSlotList:
-            lstIndx = len(gotSlotList) - 1
+    return res if res else "Not enough presenters"
 
-            # Continue in following cases :
-            # If current number of presenter is less or
-            # if current total cost is greater than the previous cost
+def getMinCost(totSlot, confDuration, csvFile):
+    data = list(getRow(csvFile))
+    result = []
+    for mxPresenterNum in range(1, totSlot+1):
+        result.extend(findCombination(mxPresenterNum, data, confDuration))
 
-            if (len(gotSlotList[lstIndx]) - 1) > curLn or \
-               totCst > gotSlotList[lstIndx][-1]:
-                return True
+    result = sortData(result)
 
-            while lstIndx >= 0:
-                # Delete from the list :
-                # If current number of presenter is greater or
-                # if current total cost is less
-                if len(gotSlotList[lstIndx]) - 1 > curLn or \
-                   totCst < gotSlotList[lstIndx][-1] :
-                    del gotSlotList[lstIndx]
-                    lstIndx -= 1
-                elif set(slotLst) == set(gotSlotList[lstIndx]):
-                    del gotSlotList[lstIndx]
-                    lstIndx -= 1
-                else:
-                    # If last element is greater then we need to check else not
-                    break
+    lstCost = None
+    res = []
+    for echSet in result:
+        tmp = [0]
+        for echPresenter in echSet:
+            tmp.insert(-1, echPresenter[0])
+            tmp[-1] += echPresenter[2]
+        if lstCost == None:
+            lstCost = tmp[-1]
+        elif lstCost < tmp[-1]:
+            break
+        res.append(tmp)
 
-        gotSlotList.append(slotLst)
-
-
-    for s1 in data:
-        if __iscontinue(s1[2], 1, s1[1], [s1[0]]):
-            continue
-
-        for s2 in data:
-            # If same data
-            if s1 == s2:
-                continue
-            totCost = s1[2] + s2[2]
-            totDur = s1[1] + s2[1]
-            if __iscontinue(totCost, 2, totDur, [s1[0], s2[0]]):
-                continue
-
-            for s3 in data:
-                # If same data
-                if s1 == s2 or s2 == s3 or s3 == s1 :
-                    continue
-                totCost = s1[2] + s2[2] + s3[2]
-                totDur = s1[1] + s2[1] + s3[1]
-                __iscontinue(totCost, 3, totDur, [s1[0], s2[0], s3[0]])
-
-
-    if gotSlotList:
-        for ei in gotSlotList:
-            print ei
-        print "-"*100
-    else:
-        print "Not enough presenters"
-    #return gotSlotList
-
-def minCost(data):
-    '''
-       Minimize the cost - Select the case which results in minimum cost for the organizer.
-       If multiple cases satisfy this scenario, then select the ones with maximum number of presenters.
-
-    data : a list of [<presenterName>, <presentationHours><int>, <cost><int>]
-    '''
-    gotSlotList = []
-
-
-    def __iscontinue(totCst, curLn, totDr, slotLst):
-        if totDr > CONFERENCE_DURATION:
-            return True
-        elif totDr < CONFERENCE_DURATION:
-            return False
-        slotLst.append(totCst)
-        # There is any data in gotSlotList only then:
-        # check for low cost and remove if the previous costs are higher
-        if gotSlotList:
-            lstIndx = len(gotSlotList) - 1
-
-            # Continue in following cases :
-            # if current total cost is greater than the previous cost
-            if totCst > gotSlotList[lstIndx][-1]:
-                return True
-
-            elif totCst == gotSlotList[lstIndx][-1] and \
-                 (len(gotSlotList[lstIndx]) - 1) > curLn:
-                return True
-
-            while lstIndx >= 0:
-                # Delete from the list :
-                # if current total cost is less
-                if totCst < gotSlotList[lstIndx][-1] :
-                    del gotSlotList[lstIndx]
-                    lstIndx -= 1
-                elif totCst == gotSlotList[lstIndx][-1] and \
-                     (len(gotSlotList[lstIndx]) - 1) < curLn:
-                    del gotSlotList[lstIndx]
-                    lstIndx -= 1
-                elif set(slotLst) == set(gotSlotList[lstIndx]):
-                    del gotSlotList[lstIndx]
-                    lstIndx -= 1
-                else:
-                    # If last element is greater then we need to check else not
-                    break
-        gotSlotList.append(slotLst)
-
-
-    for s1 in data:
-        if __iscontinue(s1[2], 1, s1[1], [s1[0]]):
-            continue
-
-        for s2 in data:
-            # If same data
-            if s1 == s2:
-                continue
-            totCost = s1[2] + s2[2]
-            totDur = s1[1] + s2[1]
-            if __iscontinue(totCost, 2, totDur, [s1[0], s2[0]]):
-                continue
-
-            for s3 in data:
-                # If same data
-                if s1 == s2 or s2 == s3 or s3 == s1 :
-                    continue
-                totCost = s1[2] + s2[2] + s3[2]
-                totDur = s1[1] + s2[1] + s3[1]
-                __iscontinue(totCost, 3, totDur, [s1[0], s2[0], s3[0]])
-
-
-    if gotSlotList:
-        for ei in gotSlotList:
-            print ei
-        print "-"*100
-    else:
-        print "Not enough presenters"
-    #return gotSlotList
+    return res if res else "Not enough presenters"
 
 
 if __name__ == '__main__':
 
     # Global Constants
     CONFERENCE_DURATION = 8
+    TOTAL_SLOT = 3
     CSV_DATA_FILE = "data.csv"
-
-    data = list(getRow())
-    maxPresenter(data)
-    minCost(data)
+    from pprint import pprint
+    print "*"*100
+    pprint(getMaxPresenterCombination(TOTAL_SLOT, CONFERENCE_DURATION, CSV_DATA_FILE))
+    print "*"*100
+    pprint(getMinCost(TOTAL_SLOT, CONFERENCE_DURATION, CSV_DATA_FILE))
+    print "*"*100
 
---
+++ test-mod.py
@@ -0,0 +1,64 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      gouri
+#
+# Created:     13-08-2014
+# Copyright:   (c) gouri 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import random
+import unittest
+from presentationScheduler import *
+
+class TestPresentationScheduler(unittest.TestCase):
+
+    def setUp(self):
+        self.testDataFile = ["data1.csv",
+                             "data2.csv",
+                             "data_not_enough_presenter.csv"]
+
+    def test_normalInput(self):
+        CONFERENCE_DURATION = 8
+        TOTAL_SLOT = 3
+        USED_DATA_FILE = self.testDataFile[0]
+
+        retMxPresenter = getMaxPresenterCombination(TOTAL_SLOT, CONFERENCE_DURATION, USED_DATA_FILE)
+        expectedRetVal = [['P1', 'P3', 'P4', 300],
+                          ['P1', 'P4', 'P5', 300],
+                          ['P3', 'P4', 'P5', 300]]
+
+        retMinCost = getMinCost(TOTAL_SLOT, CONFERENCE_DURATION, USED_DATA_FILE)
+        expectedRetVal = [['P6', 50]]
+
+    def test_not_enough_presenter(self):
+        expectedRetVal = "Not enough presenters"
+        CONFERENCE_DURATION = 8
+        TOTAL_SLOT = 3
+        USED_DATA_FILE = self.testDataFile[2]
+
+        retMxPresenter = getMaxPresenterCombination(TOTAL_SLOT, CONFERENCE_DURATION, USED_DATA_FILE)
+        self.assertEqual(retMxPresenter, expectedRetVal)
+
+        retMinCost = getMinCost(TOTAL_SLOT, CONFERENCE_DURATION, USED_DATA_FILE)
+        self.assertEqual(retMinCost, expectedRetVal)
+
+    def test_some_date_contains_no_cost(self):
+        CONFERENCE_DURATION = 8
+        TOTAL_SLOT = 3
+        USED_DATA_FILE = self.testDataFile[1]
+
+        retMxPresenter = getMaxPresenterCombination(TOTAL_SLOT, CONFERENCE_DURATION, USED_DATA_FILE)
+        expectedRetVal = [['P1', 'P3', 'P4', 300],
+                          ['P1', 'P4', 'P5', 300],
+                          ['P3', 'P4', 'P5', 300]]
+        self.assertEqual(retMxPresenter, expectedRetVal)
+
+        retMinCost = getMinCost(TOTAL_SLOT, CONFERENCE_DURATION, USED_DATA_FILE)
+        expectedRetVal = [['P6', 50]]
+        self.assertEqual(retMinCost, expectedRetVal)
+
+if __name__ == '__main__':
+    unittest.main()
